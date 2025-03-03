95891470ac3d8348d538b095ccdd6bb2d5138b6a
Diego Carvalho <diego.14crvlho@gmail.com>
2025-03-03 09:47:53 -0400

    wip

--- src/store.js
+++ src/store.js
@@ -1,112 +1,17 @@
-import { combineReducers, createStore } from "redux";
-
-const initialStateAccount = {
-  balance: 0,
-  loan: 0,
-  loanPurpose: "",
-};
-
-const initialStateCustomer = {
-  fullName: "",
-  nationalId: "",
-  createdAt: "",
-};
-
-function AccountReducer(state = initialStateAccount, action) {
-  switch (action.type) {
-    case "account/deposit":
-      return { ...state, balance: state.balance + action.payload };
-    case "account/withdraw":
-      return { ...state, balance: state.balance - action.payload };
-    case "account/requestLoan":
-      if (state.loan > 0) return state;
-      return {
-        ...state,
-        loan: action.payload.amount,
-        balance: state.balance + action.payload.amount,
-        loanPurpose: action.payload.purpose,
-      };
-    case "account/payLoan":
-      return {
-        ...state,
-        loan: 0,
-        loanPurpose: "",
-        balance: state.balance - state.loan,
-      };
-    default:
-      return state;
-  }
-}
-
-function customerReducer(state = initialStateCustomer, action) {
-  switch (action.type) {
-    case "customer/createCustomer":
-      return {
-        ...state,
-        fullName: action.payload.fullName,
-        nationalId: action.payload.nationalId,
-        createdAt: action.payload.createdAt,
-      };
-    case "customer/updateCustomer":
-      return { ...state, fullName: action.payload };
-    default:
-      return state;
-  }
-}
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import AccountReducer from "./features/accounts/AccountSlice";
+import customerReducer from "./features/customers/CustomerSlice";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const rootReducer = combineReducers({
   account: AccountReducer,
   customer: customerReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
-function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
-}
-function withdraw(amount) {
-  return { type: "account/withdraw", payload: amount };
-}
-function requestLoan(amount, purpose) {
-  return {
-    type: "account/requestLoan",
-    payload: {
-      amount,
-      purpose,
-    },
-  };
-}
-function payLoan() {
-  return { type: "account/payLoan" };
-}
-
-store.dispatch(deposit(100));
-store.dispatch(withdraw(50));
-store.dispatch(requestLoan(1000, "pay off credit card debt"));
-store.dispatch(payLoan());
-
-console.log(store.getState());
-
-function createCustomer(fullName, nationalId) {
-  return {
-    type: "customer/createCustomer",
-    payload: {
-      fullName,
-      nationalId,
-      createdAt: new Date().toISOString(),
-    },
-  };
-}
-
-function updateCustomer(fullName) {
-  return {
-    type: "customer/updateCustomer",
-    payload: fullName,
-  };
-}
-
-store.dispatch(createCustomer("Diego Carvalho Ferreira", "123456789"));
-console.log(store.getState());
-
-store.dispatch(updateCustomer("Diego Ferreira"));
-console.log(store.getState());
+export default store;
